d4d714cc504a5f22d8450ad463de47c47ce93a01
meghrathod <msr.megh@gmail.com>
2021-04-12 16:18:04 +0530

    made skeleton

---
+++ src/components/hompage.component.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TITLE</h1>
+                    <span className="subtitle">SUBTITLE</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TITLE</h1>
+                    <span className="subtitle">SUBTITLE</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TITLE</h1>
+                    <span className="subtitle">SUBTITLE</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TITLE</h1>
+                    <span className="subtitle">SUBTITLE</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">TITLE</h1>
+                    <span className="subtitle">SUBTITLE</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
