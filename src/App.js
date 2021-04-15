f9adb46e6cdf32c543ea7f1e5c9d8b7cc987647e
meghrathod <msr.megh@gmail.com>
2021-04-15 17:18:15 +0530

    Adding header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
+import Header from "./components/header/header.component"
 import HomePage from "./pages/homepage/hompage.component";
 import "./pages/homepage/homepage.styles.scss"
 import ShopPage from "./pages/shop/shop.component.jsx";
 import "./pages/shop/shop.styles.scss";
+import "./components/header/header.component.jsx"
 
 import { Route, Switch } from "react-router-dom";
 
 function App() {
     return (
         <div>
+			<Header/>
             <Switch>
                 <Route exact path="/" component={HomePage} />
 				<Route path="/shop" component={ShopPage} />
