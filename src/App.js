b2dc035bceb5b84ad7eed213da82710928e93105
meghrathod <msr.megh@gmail.com>
2021-04-15 15:20:00 +0530

    update code for withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
-import HomePage from './pages/hompage.component.jsx'
-import './pages/homepage.styles.scss'
+import "./App.css";
+import HomePage from "./pages/hompage.component.jsx";
+import "./pages/homepage.styles.scss";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+	<div>
+		<h1>HatsPage</h1>
+	</div>
+)
 
 function App() {
-  return (
-    <div>
-      <HomePage/>
-    </div>
-  );
+    return (
+        <div>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+				<Route path="/shop/hats" component={HatsPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
